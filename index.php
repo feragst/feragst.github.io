2b67d9e73550e18634d0f17f85eebac58f7c7373
Fernando Agostini <139084664+feragst@users.noreply.github.com>
2025-05-21 19:45:57 -0300

    zzzzzz

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="javascr.js"></script>
+</head>
+
+<body class="home">
+<div class="container">
+    <h1>LOJA DO GRINGO</h1>
+    <div class="button-group">
+        <a href="cadastrocliente/cadastra.html" class="btn">Cadastro de Clientes</a>
+        <a href="cadastrovendas/cadastra.html" class="btn">Cadastro de Vendas</a>
+    </div>
+    <div class="extra-button">
+        <button onclick="msgMotiv()" class="btn extra">🎉 MENSAGENS MOTIVACIONAIS DO GRINGO</button>
+    </div>
+</div>
+
+
+
+
+</body>
+
+</html>
---
+++ javascr.js
@@ -0,0 +1,25 @@
+function msgMotiv() {
+    const mensagens = [
+        " Desista!",
+        " Desista dos seus sonhos e morra!",
+        " Se a vida te der limões, tenha um ataque de panico!",
+        " Vai dar tudo errado!",
+        " Cale a boca de todos aqueles que te elogiaram!",
+        " Se você deu 90% de si e não foi suficiente, desista!",
+        "🌈 Deus não vai te deixar entrar no céu hein...",
+        " Em angola você nao pisa",
+        " Bomboclaat",
+        " Amarrem-me em um missel com destino a tel aviv",
+        " Receba! É o melhor do mundo luva de pedreiro pai",
+        " Você tá jurado de morte aqui em Doutor Ricardo - RS",
+        " Você tá jurado de morte aqui em Nova Bréscia - RS",
+        " Você tá jurado de morte aqui em Mato Leitao - RS",
+        " Uh teilao vai te pegar...."
+
+        
+
+    ];
+    
+    const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
+    alert(aleatoria);
+}
